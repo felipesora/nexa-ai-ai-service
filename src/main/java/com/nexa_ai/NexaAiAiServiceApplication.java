136da30c01756c4f22a7d9c576c9704c5b5747f8
felipesora <felipeusora@gmail.com>
2026-07-06 13:22:45 -0300

    Começando serviço de AI

---
+++ src/main/java/com/nexa_ai/NexaAiAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nexa_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaAiAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexaAiAiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexa_ai/NexaAiAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexa_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexaAiAiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
